575765fafe7b4dfc8b897f88880253a9367f446c
tana9373 <83992274+tana9373@users.noreply.github.com>
2021-10-22 11:59:09 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -1,14 +1,15 @@
-from callsmusic.callsmusic import client as USER
+from callsmusic.callsmusic import client as tushar
 
 from pyrogram import filters
 
 from pyrogram.types import Chat, Message, User
 
-@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+@tushar.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 
-async def pmPermit(client: USER, message: Message):
+async def pmPermit(client: tushar, message: Message):
 
   await USER.send_message(message.chat.id,"Hello, I'm lovely official **music assistant of @LOVELYR_OBOT.**\n\n❗️ **notes:**\n\n⫸ don't spam message.\n⫸ don't send me anything confidential\n\n⨀ Join to @ABOUTVEDMAT \n⨀ Join to @LOVELYAPPEAL\n\n🤴 Dev: @TUSHAR204\n\n👩🏻‍🔧 If you want me join to your group, send here your group link, I will joined as soon as possible.\n\n")
    
   return                        
 
+#haters ma chudao 😏😏😒
